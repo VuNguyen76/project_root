855b8ec8f9c2aa4288a8fba7c8f2b98cd6c2f306
Châm <2251120403@ut.edu.vn>
2024-11-12 22:14:48 +0700

    Delete project_root directory

--- project_root/assets/js/script.js
+++
@@ -1,2 +0,0 @@
-// Bạn có thể thêm các logic JavaScript để xử lý giao diện hoặc các yêu cầu khác
-console.log("JavaScript is connected!");
--- project_root/wbingosite.com/resources/js/card.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#chonhet').click(function() {
-        var status = this.checked;
-        $('input[name=chon]').each(function() {
-            this.checked = status;
-        });
-    });
-});
